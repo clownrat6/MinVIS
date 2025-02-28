c5e1554aea1de271d11b93baf07868d5e2c00d8f
clownrat6 <cyanlaser@stu.pku.edu.cn>
2025-02-28 12:07:10 +0800

    Add hqytvis dataset.

--- minvis/data_video/datasets/builtin.py
+++ minvis/data_video/datasets/builtin.py
@@ -81,9 +81,32 @@ def register_all_ovis(root):
         )
 
 
+# ==== Predefined splits for HQYTVIS ===========
+_PREDEFINED_SPLITS_HQYTVIS = {
+    "hqytvis_train": ("ytvis_2019/train/JPEGImages",
+                      "hqytvis/ytvis_hq-train.json"),
+    "hqytvis_val":   ("ytvis_2019/train/JPEGImages",
+                      "hqytvis/ytvis_hq-val.json"),
+    "hqytvis_test":  ("ytvis_2019/train/JPEGImages",
+                      "hqytvis/ytvis_hq-test.json"),
+}
+
+
+def register_all_hqytvis(root):
+    for key, (image_root, json_file) in _PREDEFINED_SPLITS_HQYTVIS.items():
+        # Assume pre-defined datasets live in `./datasets`.
+        register_ytvis_instances(
+            key,
+            _get_ytvis_2019_instances_meta(),
+            os.path.join(root, json_file) if "://" not in json_file else json_file,
+            os.path.join(root, image_root),
+        )
+
+
 if __name__.endswith(".builtin"):
     # Assume pre-defined datasets live in `./datasets`.
     _root = os.getenv("DETECTRON2_DATASETS", "datasets")
     # register_all_ytvis_2019(_root)
     # register_all_ytvis_2021(_root)
     register_all_ovis(_root)
+    register_all_hqytvis(_root)
